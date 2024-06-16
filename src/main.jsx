4e283bb50904637ee90c59cb9fca20fee521d7df
Anjali Kushwaha <69588336+anjali-kushwaha@users.noreply.github.com>
2024-06-16 16:46:06 +0530

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-// ROUTER
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.scss";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Space-Tourism">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
